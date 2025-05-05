e1c6d99d081ef0c2fe232b2dd78ce2fb0b9cfa39
baldersd <baldersd@hotmail.com>
2025-05-05 15:36:06 +0100

    main app

--- main.go
+++ main.go
@@ -1,6 +1,29 @@
 package main
 
+import "main/docs"
 import "github.com/gin-gonic/gin"
+import "github.com/swaggo/gin-swagger" // gin-swagger middleware
+import "github.com/swaggo/files"
+
+// @title           GOlang Example RESTful API
+// @version         1.0
+// @description     This is a sample web service.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /ping
+
+// @securityDefinitions.basic  BasicAuth
+
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/
 
 func main() {
     r := gin.Default()
